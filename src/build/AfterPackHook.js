b9b046944feacb69af3d6a2aa9cadfc7f6a435a5
GG2015 <inquiry.globalgamer2015@gmail.com>
2022-10-12 13:43:14 -0400

    Cleanup

--- src/build/AfterPackHook.js
+++ src/build/AfterPackHook.js
@@ -12,3 +12,47 @@ exports.default = async function(context) {
         }
     }
 }
+
+/*
+
+"build": {
+		"afterPack": "./build/AfterPackHook.js",
+		"appId": "com.GG2015.OdyseeChatterBot",
+		"extraFiles": [
+			{
+				"from": "./data/languages.json",
+				"to": "./data/languages.json"
+			},
+			{
+				"from": "./app-update.yml",
+				"to": "./resources/app-update.yml"
+			},
+			{
+				"from": "./ENDUSER_LICENSE",
+				"to": "./LICENSE.odyseechatter_enduser.txt"
+			}
+		],
+		"win": {
+			"target": [
+				"nsis"
+			],
+			"icon": "build/icon.ico"
+		},
+		"nsis": {
+			"include": "build/installer.nsh",
+			"installerIcon": "build/icon.ico",
+			"uninstallerIcon": "build/icon.ico",
+			"license": "ENDUSER_LICENSE",
+			"runAfterFinish": false,
+			"allowToChangeInstallationDirectory": true,
+			"oneClick": false,
+			"perMachine": false,
+			"multiLanguageInstaller": true,
+			"createDesktopShortcut": true,
+			"createStartMenuShortcut": true,
+			"menuCategory": true,
+			"deleteAppDataOnUninstall": false
+		}
+	}
+
+*/
